8888a224f18af15c5a156467bcfae8dc8a56e7e3
Maurits van der Schee <maurits@vdschee.nl>
2024-04-14 13:42:41 +0200

    Initial release

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ if [ ! -f pcodes-geo.zip ]; then
   wget berthub.eu/bagconv/pcodes-geo.zip 
 fi
 zcat pcodes-geo.zip | php minimize.php | 7z a postcodes-nl-geo.7z -sipostcodes-nl-geo.csv
-#newTag=$(date '+v%y.%m')
+newTag=$(date '+v%y.%m')
 #gh release delete $newTag
-#gh release create $newTag postcodes-nl-geo.php postcodes-nl-geo.7z postcodes-nl-geo.sh
-#rm pcodes-geo.zip
+gh release create $newTag postcodes-nl-geo.php postcodes-nl-geo.7z postcodes-nl-geo.sh
+rm pcodes-geo.zip
