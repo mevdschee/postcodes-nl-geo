1fd3a9069dc0bc8dcfe8ceb5dd73f4e31a6936ec
Maurits van der Schee <maurits@vdschee.nl>
2024-05-03 17:29:10 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 if [ ! -f postcodes-nl-geo.7z ]; then
-  if [ -f ../bagconv/dist/postcodes-nl-geo.7z ]; then
-    cp ../bagconv/dist/postcodes-nl-geo.7z .
-  else
-    if [ ! -f pcodes-geo.zip ]; then
-      wget berthub.eu/bagconv/pcodes-geo.zip 
-    fi
-    zcat pcodes-geo.zip | php minimize.php | 7z a postcodes-nl-geo.7z -sipostcodes-nl-geo.csv
-    #rm pcodes-geo.zip
-  fi
+  cp ../bagconv/dist/postcodes-nl-geo.7z .
 fi
 newTag=$(date '+v%y.%m')
 #gh release delete $newTag
